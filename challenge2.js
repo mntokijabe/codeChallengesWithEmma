25d30baceb645dc584d3381e6b5505b79f8437f7
Elwood Nelson <elwoodnelson@gmail.com>
2025-01-25 10:40:19 -0600

    finished challenge 2

---
+++ challenge2.js
@@ -0,0 +1,9 @@
+function calculateTime(battery, charger) {
+  return parseFloat(
+    (
+      (battery * 0.85) / charger +
+      (battery * 0.1) / (charger * 0.5) +
+      (battery * 0.05) / (charger * 0.2)
+    ).toFixed(2)
+  );
+}
